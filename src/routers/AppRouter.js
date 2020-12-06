d94a36d3db5d4b5f98f3e611c6987f163614e0d9
Aleksandar Mihajlovic <36760259+Slapman123@users.noreply.github.com>
2020-12-06 21:08:46 +0100

    fixed

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import {BrowserRouter,Route,Switch} from 'react-router-dom'
-import AddExpense from "../pages/AddExpense"
-import EditExpense from "../pages/EditExpense"
-import ExpenseDashboard from "../pages/ExpenseDashboard"
-import Header from "../pages/Header"
-import HelpPage from "../pages/HelpPage"
-import NotFound from "../pages/NotFound"
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 
 const AppRouter = () => (
     <BrowserRouter>
         <div>
             <Header/>
             <Switch>
-                <Route path="/" component={ExpenseDashboard} exact={true}/>
-                <Route path="/create" component={AddExpense} />
-                <Route path="/edit" component={EditExpense} />
-                <Route path="/help" component={HelpPage} />
-                <Route component={NotFound}/>
+                
             </Switch>
         </div>
     </BrowserRouter>
